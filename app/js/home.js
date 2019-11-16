c34cbd34fa0c8a670fab9f0f37bccf48dd44b3d1
Ivan Rylskiy <ivan.rylskiy@gmail.com>
2019-11-16 20:22:57 +0700

    load home on click

--- app/js/home.js
+++ app/js/home.js
@@ -3,77 +3,98 @@
 // При каждой загрузке страницы товары, попадающие в блок, должны выбираться произвольно
 
 export function home(products) {
-    let productsLength = products.length;
-    console.log(productsLength, 'количество продуктов');
-
-    function getRandomInt(min, max) {
-        return Math.floor(Math.random() * (max - min + 1)) + min;
+    let fakeChildCreate = document.createElement('div');
+    fakeChildCreate.className = 'fake-child';
+    if (document.body.children[0].children.length == 4) {
+        document.body.children[0].replaceChild(fakeChildCreate, document.body.children[0].children[2]);
     }
+    if (window.location.href.indexOf('index.html') != -1) {
+        let fakeChild = document.querySelector('.fake-child');
+        let homeSectionCreate = document.createElement('section');
+        homeSectionCreate.className = 'home';
+        homeSectionCreate.innerHTML = `<div class="top home__top"></div>
+                                        <div class="bot home__bot">
+                                            <div class="left home__left"></div>
+                                            <div class="right home__right"></div>
+                                        </div>`;
 
-    let randomProductsLength = getRandomInt(0, productsLength);
-    console.log(randomProductsLength, 'рандомное количество продуктов');
+        document.body.children[0].replaceChild(homeSectionCreate, fakeChild);
 
-    const topBlock = document.querySelector('.top');
-    const leftBlock = document.querySelector('.left');
-    const rightBlock = document.querySelector('.right');
-    let cycleRandomElement = randomProductsLength;
+        let productsLength = products.length;
+        console.log(productsLength, 'количество продуктов');
 
-    const cardSizeTextMd = 'card-md';
-    const cardSizeTextLg = 'card-lg';
-    const cardSizeTextSm = 'card-sm';
+        function getRandomInt(min, max) {
+            return Math.floor(Math.random() * (max - min + 1)) + min;
+        }
 
-    function drawCard(block, cardSizeText) {
-        let randomElement = getRandomInt(0, cycleRandomElement - 1);
-        const element = products.splice(randomElement, 1)[0];
-        let cardSize = document.createElement('div');
-        cardSize.className = `${cardSizeText} top__${cardSizeText}`;
-        cardSize.innerHTML = `<div class="${cardSizeText}__top">
-                                <img src="${element.src}" alt="${cardSizeText}__img" class="${cardSizeText}__img">
-                            </div>
-                            <div class="${cardSizeText}__bot">
-                                <div class="desc ${cardSizeText}__desc">
-                                    <p class="supp desc__supp">${element.suppliersName}</p>
-                                    <p class="prod desc__prod">${element.productName}</p>
-                                    <p class="price desc__price">${element.price}</p>
+        let randomProductsLength = getRandomInt(0, productsLength);
+        console.log(randomProductsLength, 'рандомное количество продуктов');
+
+        const topBlock = document.querySelector('.top');
+        const leftBlock = document.querySelector('.left');
+        const rightBlock = document.querySelector('.right');
+        let cycleRandomElement = randomProductsLength;
+
+        const cardSizeTextMd = 'card-md';
+        const cardSizeTextLg = 'card-lg';
+        const cardSizeTextSm = 'card-sm';
+
+        function drawCard(block, cardSizeText) {
+            let randomElement = getRandomInt(0, cycleRandomElement - 1);
+            const element = products.splice(randomElement, 1)[0];
+            let cardSize = document.createElement('div');
+            cardSize.className = `${cardSizeText} top__${cardSizeText}`;
+            cardSize.innerHTML = `<div class="${cardSizeText}__top">
+                                    <img src="${element.src}" alt="${cardSizeText}__img" class="${cardSizeText}__img">
                                 </div>
-                                <svg class="more-info" viewbox="0 0 12 20">
-                                    <use xlink:href="assets/img/icons.svg#more-info"></use>
-                                </svg>
-                            </div>`;
-        block.appendChild(cardSize);
-        cycleRandomElement--;
-    }
+                                <div class="${cardSizeText}__bot">
+                                    <div class="desc ${cardSizeText}__desc">
+                                        <p class="supp desc__supp">${element.suppliersName}</p>
+                                        <p class="prod desc__prod">${element.productName}</p>
+                                        <p class="price desc__price">${element.price}</p>
+                                    </div>
+                                    <svg class="more-info" viewbox="0 0 12 20">
+                                        <use xlink:href="assets/img/icons.svg#more-info"></use>
+                                    </svg>
+                                </div>`;
+            block.appendChild(cardSize);
+            cycleRandomElement--;
+        }
 
-    function drawHome() {
-        for (let i = 0; i < randomProductsLength; i++) {
-            if (i < 3) {
-                drawCard(topBlock, cardSizeTextMd);
-            }
+        function drawHome() {
+            for (let i = 0; i < randomProductsLength; i++) {
+                if (i < 3) {
+                    drawCard(topBlock, cardSizeTextMd);
+                }
 
-            if (i == 3) {
-                drawCard(leftBlock, cardSizeTextLg);
-            }
+                if (i == 3) {
+                    drawCard(leftBlock, cardSizeTextLg);
+                }
 
-            if ((i > 3) && (i < 8)) {
-                drawCard(rightBlock, cardSizeTextSm);
-            }
+                if ((i > 3) && (i < 8)) {
+                    drawCard(rightBlock, cardSizeTextSm);
+                }
 
-            if ((i > 7) && (i < 12)) {
-                drawCard(leftBlock, cardSizeTextSm);
-            }
+                if ((i > 7) && (i < 12)) {
+                    drawCard(leftBlock, cardSizeTextSm);
+                }
 
-            if (i == 12) {
-                drawCard(rightBlock, cardSizeTextLg);
-            }
+                if (i == 12) {
+                    drawCard(rightBlock, cardSizeTextLg);
+                }
 
-            if ((i > 12) && (i < 15)) {
-                drawCard(leftBlock, cardSizeTextSm);
-            }
+                if ((i > 12) && (i < 15)) {
+                    drawCard(leftBlock, cardSizeTextSm);
+                }
 
-            if ((i > 14) && (i < 17)) {
-                drawCard(rightBlock, cardSizeTextSm);
+                if ((i > 14) && (i < 17)) {
+                    drawCard(rightBlock, cardSizeTextSm);
+                }
             }
         }
+        drawHome();
+    } else {
+        let homeSection = document.querySelector('.home');
+        document.body.children[0].replaceChild(fakeChildCreate, homeSection);
     }
-    drawHome();
 }
--- app/js/script.js
+++ app/js/script.js
@@ -3,5 +3,14 @@ import { request } from './request.js';
 import { menu } from './menu.js';
 
 const urlHome = 'http://127.0.0.1:5500/JS/projectOne/app/data/home.json';
-request(urlHome, home);
+window.onload = function() {
+    request(urlHome, home);
+};
+let allLinks = document.querySelectorAll('a');
+allLinks.forEach(elements => {
+    elements.addEventListener('click', (event) => {
+        event.preventDefault();
+        request(urlHome, home);
+    });
+});
 menu();
