ba5edb48c3c624dc1cf79a6226362503afd1699a
Ivan Rylskiy <ivan.rylskiy@gmail.com>
2019-11-21 22:51:35 +0700

    cart is fully rendering now from localStorage

--- app/js/cart.js
+++ app/js/cart.js
@@ -54,4 +54,48 @@ export function cart() {
         cartSectionCreate.appendChild(clone);
     }
     cartPrep();
+    console.log(JSON.parse(localStorage.getItem('items')));
+
+    function cartDraw() {
+        let cartItemTemplate = document.querySelector('#cart-item'),
+            deleteCartItem = cartItemTemplate.content.querySelector('.icon__cross'),
+            imgCartItem = cartItemTemplate.content.querySelector('.product__image'),
+            prodCartItem = cartItemTemplate.content.querySelector('.product__name'),
+            suppCartItem = cartItemTemplate.content.querySelector('.product__supplier'),
+            priceCartItem = cartItemTemplate.content.querySelector('.cart__td_price'),
+            quanCartItem = cartItemTemplate.content.querySelector('.cart__quan'),
+            priceSumCartItem = cartItemTemplate.content.querySelector('.cart__td_sum');
+
+        let cartBody = document.querySelector('.cart__tbody');
+
+        let localStorageItems = JSON.parse(localStorage.getItem('items'));
+        if (localStorageItems != null) {
+            for (let i = 0; i < localStorageItems.length; i++) {
+                deleteCartItem.setAttribute('data-id', localStorageItems[i].id);
+                imgCartItem.src = localStorageItems[i].src;
+                prodCartItem.textContent = localStorageItems[i].productName;
+                suppCartItem.textContent = localStorageItems[i].suppliersName;
+                priceCartItem.textContent = '$'.concat(localStorageItems[i].price);
+                quanCartItem.value = localStorageItems[i].quantity;
+                priceSumCartItem.textContent = '$'.concat(+(priceCartItem.textContent.split('$')[1]) * +(quanCartItem.value));
+
+                let clone = document.importNode(cartItemTemplate.content, true);
+                cartBody.appendChild(clone);
+            }
+        } else {
+            cartBody.textContent = 'Корзина пуста';
+        }
+
+        let sumItems = document.querySelectorAll('.cart__td_sum');
+        let subtotal = document.querySelector('.cart__subtotal');
+        let shipping = document.querySelector('.cart__shipping');
+        let total = document.querySelector('.cart__total');
+        let subtotalRender = 0;
+        sumItems.forEach(elem => {
+            subtotalRender += +(elem.textContent.split('$')[1]);
+        });
+        subtotal.textContent = '$'.concat(subtotalRender);
+        total.textContent = '$'.concat(+(shipping.textContent.split('$')[1]) + +(subtotal.textContent.split('$')[1]));
+    }
+    cartDraw();
 }
